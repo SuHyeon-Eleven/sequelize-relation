52428d992000f476087f5f631ecc684957aa8679
suhyeon <minsoohyeon1234@gmail.com>
2023-03-06 14:57:04 +0900

    sequelize practice

---
+++ routes/posts.route.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const authMiddleware = require('../middlewares/auth-middleware')
+const { Posts }= require('../models')
+// 게시글 생성 API
+router.post('/posts', authMiddleware, async (req, res) => {
+    const { userId } = res.locals.user
+    const { title, content } = req.body
+    const post = await Posts.create({
+        UserId: userId,
+        title, content
+    })
+    return res.status(201).json({ data: post })
+})
+
+// 게시글 목록 조회 API
+router.get('/posts', async (req,res)=>{
+    const posts = await Posts.findAll({
+        attributes : ['postId','title','createdAt','updatedAt'],
+        order: [['createdAt', 'DESC']]
+    })
+
+    return res.status(200).json({data:posts})
+})
+
+// 게시글 상세 조회 API
+router.get('/posts/:postId', async(req,res)=>{
+    const {postId} = req.params
+    const post = await Posts.findOne({
+        attributes:['postId', 'title','createdAt','UpdatedAt'],
+        where: {postId}
+    })
+    return res.status(200).json({data:post})
+})
+module.exports = router
